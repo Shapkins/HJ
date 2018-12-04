4078137e377a9a42ad8db4bccedfc89941f79cfe
Stanislav Shapkin <shapkin359@gmail.com>
2018-12-04 23:31:15 +0300

    внесены исправления в задания 2.2 и 2.3

--- hj-homeworks/html-element-collection/audioplayer/player.js
+++ hj-homeworks/html-element-collection/audioplayer/player.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const playlist = ['./mp3/LA Chill Tour.mp3', './mp3/LA Fusion Jam.mp3', './mp3/This is it band.mp3'];
-const controls = document.getElementsByClassName('fa');
+const controls = document.getElementsByTagName('button');
 let phase = 300;
 
 for(let control of controls) {
@@ -9,31 +9,37 @@ for(let control of controls) {
 }
 
 function player() {
-  if (this.classList.contains('fa-backward')) {
+  if (this.classList.contains('back')) {
     phase--;
-    document.getElementsByTagName('audio')[0].src = playlist[phase % 3];
-    document.getElementsByClassName('title')[0].title = playlist[phase % 3].slice(6, playlist[phase % 3].length - 4);
+    changeTrack();
   }
-  if (this.classList.contains('fa-play')) {
-    document.getElementsByTagName('audio')[0].play();
-    document.getElementsByClassName('mediaplayer')[0].classList.toggle('play');
+  if (this.classList.contains('playstate')) {
+    if (!(document.getElementsByClassName('mediaplayer')[0].classList.contains('play'))) {
+      document.getElementsByClassName('mediaplayer')[0].classList.toggle('play');
+      document.getElementsByTagName('audio')[0].play();
+    } else {
+      document.getElementsByClassName('mediaplayer')[0].classList.toggle('play');
+      document.getElementsByTagName('audio')[0].pause();
+    }
   }
-  if (this.classList.contains('fa-stop')) {
+  if (this.classList.contains('stop')) {
     document.getElementsByTagName('audio')[0].pause();
     document.getElementsByTagName('audio')[0].currentTime = 0;
+    if (document.getElementsByClassName('mediaplayer')[0].classList.contains('play')) {
+      document.getElementsByClassName('mediaplayer')[0].classList.toggle('play');
+    }
   }
-  if (this.classList.contains('fa-forward')) {
+  if (this.classList.contains('next')) {
     phase++;
-    document.getElementsByTagName('audio')[0].src = playlist[phase % 3];
-    document.getElementsByClassName('title')[0].title = playlist[phase % 3].slice(6, playlist[phase % 3].length - 4);
-  }
-  if (this.classList.contains('fa-pause')) {
-    document.getElementsByTagName('audio')[0].pause();
-    document.getElementsByClassName('mediaplayer')[0].classList.toggle('play');
-  }     
+    changeTrack();
+  }   
 }
 
 
-function back() {
-
+function changeTrack() {
+  document.getElementsByClassName('title')[0].title = playlist[phase % 3].slice(6, playlist[phase % 3].length - 4);
+  document.getElementsByTagName('audio')[0].src = playlist[phase % 3];
+  if (document.getElementsByClassName('mediaplayer')[0].classList.contains('play')) {
+    document.getElementsByTagName('audio')[0].play();
+  }
 }
--- hj-homeworks/html-element-collection/drum-machine/drummachine.js
+++ hj-homeworks/html-element-collection/drum-machine/drummachine.js
@@ -1,33 +1,22 @@
 'use strict';
 
 const sounds = document.getElementsByClassName('drum-kit__drum');
+const keys = ['key-clap', 'key-hihat', 'key-kick', 'key-openhat', 'key-boom', 'key-ride'];
 
 for(let sound of sounds) {
   sound.onclick = drummer;
 }
 
 function drummer() {
-  if (this.classList.contains('key-clap')) {
-    player.call(this);
-  }
-  if (this.classList.contains('key-hihat')) {
-    player.call(this);
-  }
-  if (this.classList.contains('key-kick')) {
-    player.call(this);
-  }
-  if (this.classList.contains('key-openhat')) {
-    player.call(this);
-  }
-  if (this.classList.contains('key-boom')) {
-    player.call(this);
-  }
-  if (this.classList.contains('key-ride')) {
-    player.call(this);
+  for(let key of keys) {
+    if (this.classList.contains(key)) {
+      player.call(this);
+    }
   }
 }
 
 function player() {
   let players = this.getElementsByTagName('audio');
+  players[0].currentTime = 0;
   players[0].play();
 }
