f02d8bbeb792a14534b6496a332d3ada2077d784
Stanislav Shapkin <shapkin359@gmail.com>
2018-12-17 20:53:14 +0300

    добавил дз 8

--- hj-homeworks/event-bubbling-capturing/shop/js/cart.js
+++ hj-homeworks/event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,13 @@
 'use strict';
+
+list.addEventListener('click', buying);
+
+function buying(event) {
+  if (event.target.className === 'add-to-cart') {
+    let item = {
+      title: event.target.dataset.title, 
+      price: event.target.dataset.price
+    };
+    addToCart(item);
+  }
+}
--- hj-homeworks/event-bubbling-capturing/table/js/sort.js
+++ hj-homeworks/event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.className === 'prop__name') {
+    table.dataset.sortBy = event.target.dataset.propName;
+    if (event.target.dataset.dir === undefined) {
+      event.target.dataset.dir = 1;
+    } else {
+      event.target.dataset.dir *= -1;
+    }
+  }
+  sortTable(table.dataset.sortBy, event.target.dataset.dir)
 }
