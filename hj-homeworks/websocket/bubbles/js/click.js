26a63f6b98cd236c9a54ca612539db54896959c6
Stanislav Shapkin <shapkin359@gmail.com>
2019-01-14 18:32:30 +0300

    web-socket задания 1 и 2

--- hj-homeworks/websocket/bubbles/js/click.js
+++ hj-homeworks/websocket/bubbles/js/click.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+let clickData = {};
+let jsonData;
+
+connection.addEventListener('open', showBubbles(connection));
+document.addEventListener('click', sendClick);
+
+function sendClick(event) {
+  clickData.x = event.clientX;
+  clickData.y = event.clientY;
+  jsonData = JSON.stringify(clickData);
+  connection.send(jsonData);
+}
--- hj-homeworks/websocket/counter/js/counter.js
+++ hj-homeworks/websocket/counter/js/counter.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counter = document.querySelector('.counter');
+const errorTag = document.querySelector('output.errors');
+if (localStorage.connections === undefined) {
+  localStorage.connections = 0;
+};
+if (localStorage.errors === undefined) {
+  localStorage.errors = 0;
+}
+
+
+
+window.addEventListener('beforeupload', () => {
+  connection.close(1000, 'Страница закрыта, работа завершена');
+  localStorage.connections--;
+  updatePage();
+});
+
+connection.addEventListener('open', () => {
+  localStorage.connections++;
+  updatePage();
+});
+
+connection.addEventListener('error', () => {
+  localStorage.errors++;
+  updatePage();
+});
+
+function updatePage() {
+  counter.textContent = localStorage.connections;
+  errorTag.textContent = localStorage.errors;
+}
