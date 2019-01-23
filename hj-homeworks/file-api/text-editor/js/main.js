686f866f35b145f5d99fdc116cd9e3bc9378f56a
Stanislav Shapkin <shapkin359@gmail.com>
2019-01-23 23:34:20 +0300

    добавлено дз по теме File api (задания 1 и 2)

--- hj-homeworks/file-api/text-editor/js/main.js
+++ hj-homeworks/file-api/text-editor/js/main.js
@@ -5,6 +5,7 @@ const throttle = ( handler, ms ) => {
     timeout = setTimeout( handler, ms );
   }
 };
+
 class TextEditor {
   constructor( container, storageKey = '_text-editor__content' ) {
     this.container = container;
@@ -15,27 +16,60 @@ class TextEditor {
     this.registerEvents();
     this.load( this.getStorageData());
   }
+
   registerEvents() {
-    const save = throttle( this.save.bind( this ), 1000 );
+    const save = throttle( this.save.bind( this ), 1000 ),
+    showHint = this.showHint.bind(this),
+    loadFile = this.loadFile.bind(this);
+
     this.contentContainer.addEventListener( 'input', save );
+    this.container.addEventListener('dragover', showHint);
+    this.container.addEventListener('drop', loadFile);
+    //this.container.addEventListener('dropleave', this.hideHint.bind(this));
   }
-  loadFile( e ) {
+
+  loadFile(event) {
+    event.preventDefault();
+    const files = Array.from(event.dataTransfer.files);
+    if (files[0].type === "text/plain") {
+      this.hideHint();
+      this.setFilename(files[0].name);
+      this.readFile(files[0]);
+    } else {
+      alert('должен быть один текстовый файл');
+      this.hideHint();
+    }
   }
-  readFile( file ) {
+
+  readFile(file) {
+    const text = new FileReader();
+    text.addEventListener('load', () => {
+      this.contentContainer.value = event.target.result;
+    });
+    text.readAsText(file);
   }
-  setFilename( filename ) {
+
+  setFilename(filename) {
     this.filenameContainer.textContent = filename;
   }
-  showHint( e ) {
+
+  showHint(event) {
+    event.preventDefault();
+    this.hintContainer.classList.add('text-editor__hint_visible');
   }
+
   hideHint() {
+    this.hintContainer.classList.remove('text-editor__hint_visible');
   }
+
   load( value ) {
     this.contentContainer.value = value || '';
   }
+
   getStorageData() {
     return localStorage[ this.storageKey ];
   }
+
   save() {
     localStorage[ this.storageKey ] = this.contentContainer.value;
   }
